6d2e33c9f45bc4546ac12970408d406bb449c33a
Daniel Liu <liudaniel@qq.com>
2023-09-26 17:19:12 +0800

    create upgradeable contracts

---
+++ contracts/TestTokenV1.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.13;
+
+// ==================== External Imports ====================
+
+import { ERC20Upgradeable } from "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import { Initializable } from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+
+contract TestTokenV1 is ERC20Upgradeable {
+    function initialize() public initializer {
+        __ERC20_init("Test Token", "TTK");
+        _mint(msg.sender, 1000 ether);
+    }
+
+    function version() public pure returns (string memory) {
+        return "v1.0";
+    }
+
+    function mint(address to, uint256 amount) external {
+        _mint(to, amount);
+    }
+}
